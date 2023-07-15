af9113a50f1fa3acd1e41550bd823e7b3210c0f7
haszanakauna <haszankauna@gmail.com>
2023-07-15 20:52:46 +0100

    added the dog items file and dog id

---
+++ src/components/DogItems.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    class="bg-white shadow rounded-xl hover:scale-105 transition-all"
+  >
+    <router-link :to="{ name: 'dogDetails', params: { id: dog.idDog } }">
+      <img
+        :src="dog.strDogThumb"
+        :alt="dog.strDog"
+        class="rounded-t-xl w-full h-48 object-cover"
+      />
+    </router-link>
+    <div class="p-3">
+      <h3 class="font-bold">{{ dog.strDog }}</h3>
+      <p class="mb-4">
+        {{ $filters.truncateWords(dog.strInstructions, 20) }}
+      </p>
+      <div class="flex items-center justify-between">
+        <MoreButton :href="dog.strMore" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+//import More from './More.vue';
+
+const { dog } = defineProps({
+  dog: {
+    required: true,
+    type: Object
+  }
+})
+
+</script>
---
+++ src/components/Dogs.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-5 px-8">
+    <DogItem v-for="dog of dogs" :key="dog.idDog" :dog="dog" />
+  </div>
+  <div v-if="!dogs.length" class="flex justify-center text-gray-600 p-8">
+    There are no Dogs
+  </div>
+</template>
+
+<script setup>
+import DogItem from './DogItems.vue';
+
+const { dogs } = defineProps({
+  dogs: {
+    required: true,
+    type: Array,
+  }
+})
+
+</script>
