e083fd63a80ac02b7493d4cbd6bafd45ce953a44
haszanakauna <haszankauna@gmail.com>
2023-07-15 22:31:59 +0100

    done with the more and navbar file

---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="bg-white shadow h-16 flex justify-between items-stretch">
+    <router-link
+      :to="{ name: 'home' }"
+      class="inline-flex items-center h-full px-5 text-orange-500 font-bold"
+    >
+      Home
+    </router-link>
+    <div class="flex items-center gap-1">
+      <router-link
+        :to="{ name: 'byName' }"
+        class="inline-flex items-center px-2 h-full transition-colors hover:bg-orange-500 hover:text-white"
+      >
+        Search Dogs
+      </router-link>
+      <router-link
+        :to="{ name: 'byLetter' }"
+        class="inline-flex items-center px-2 h-full transition-colors hover:bg-orange-500 hover:text-white"
+      >
+        Dogs By Letter
+      </router-link>
+      <router-link
+        :to="{ name: 'breeds' }"
+        class="inline-flex items-center px-2 h-full transition-colors hover:bg-orange-500 hover:text-white"
+      >
+        Dogs By breed
+      </router-link>
+    </div>
+  </header>
+</template>
